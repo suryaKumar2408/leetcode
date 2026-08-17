f9346e10ac6825764e1bc6c0e00c2580a1e9d8b6
Surya Kumar <suryashukla3284@gmail.com>
2026-08-17 19:56:36 +0530

    Time: 0 ms (100%), Space: 9.3 MB (19.9%) - LeetHub

---
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int solve(string &s, int i, long long num, int sign) {
+        // Base case: string khatam ya non-digit
+        if (i >= s.size() || s[i] < '0' || s[i] > '9') {
+            return sign * num;
+        }
+
+        int digit = s[i] - '0';
+        if (num > (2147483647LL - digit) / 10) {
+            if (sign == 1)
+                return 2147483647;
+            else
+                return -2147483648LL;
+        }
+
+        num = num * 10 + digit;
+
+        return solve(s, i + 1, num, sign);
+    }
+
+    int myAtoi(string s) {
+        int i = 0;
+        int sign = 1;
+        while (i < s.size() && s[i] == ' ') {
+            i++;
+        }
+        if (i < s.size() && s[i] == '-') {
+            sign = -1;
+            i++;
+        }
+        else if (i < s.size() && s[i] == '+') {
+            i++;
+        }
+
+        return solve(s, i, 0, sign);
+    }
+};
