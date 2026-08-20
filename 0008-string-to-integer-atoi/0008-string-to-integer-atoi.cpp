63139b0803ad0e78e3f073bc3a5ef8ce74200d4c
Surya Kumar <suryashukla3284@gmail.com>
2026-08-20 14:31:00 +0530

    Time: 3 ms (13.12%), Space: 10.3 MB (5.03%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -1,38 +1,33 @@
 class Solution {
 public:
-    int solve(string &s, int i, long long num, int sign) {
-        // Base case: string khatam ya non-digit
-        if (i >= s.size() || s[i] < '0' || s[i] > '9') {
-            return sign * num;
+    int solve(string s, int i, int num, int sign){
+        if(i>=s.size()||s[i]<'0'||s[i]>'9'){
+            return num*sign;
         }
-
-        int digit = s[i] - '0';
+        int digit =s[i]-'0';
         if (num > (2147483647LL - digit) / 10) {
             if (sign == 1)
                 return 2147483647;
             else
                 return -2147483648LL;
         }
-
-        num = num * 10 + digit;
-
+        num=num*10+digit;
         return solve(s, i + 1, num, sign);
     }
-
     int myAtoi(string s) {
-        int i = 0;
-        int sign = 1;
-        while (i < s.size() && s[i] == ' ') {
+        int i=0;
+        int sign=1;
+        int num=0;
+        while(i<s.size()&&s[i]==' '){
             i++;
         }
-        if (i < s.size() && s[i] == '-') {
-            sign = -1;
+        if(i<s.size()&&s[i]=='-'){
+            sign=-1;
+            i++;
+        }else if(i<s.size()&&s[i]=='+'){
             i++;
         }
-        else if (i < s.size() && s[i] == '+') {
-            i++;
-        }
-
-        return solve(s, i, 0, sign);
+        return solve(s,i,num,sign);
+        
     }
 };
